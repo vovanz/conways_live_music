3ee39a415203a8e23ad4d3aae42c09038684f482
vovanz <vovanzagorodsky@gmail.com>
2015-10-18 22:04:53 +0500

    live draft

--- src/js/life.js
+++ src/js/life.js
@@ -10,18 +10,22 @@ class Life {
         this.died = new ExtendedSet();
         for (let cell of this.state) {
             let alive_neighbours = ExtendedSet.intersect(cell.neighbours, this.state);
-            if(alive_neighbours.size < 2 || alive_neighbours > 3) {
+            if (alive_neighbours.size < 2 || alive_neighbours > 3) {
                 this.died.add(cell)
             }
             let all_neighbours = cell.neighbours;
             let dead_neighbours = all_neighbours.diff_update(alive_neighbours);
-            for(let dead_neighbour of dead_neighbours) {
+            for (let dead_neighbour of dead_neighbours) {
                 let parents = ExtendedSet.intersect(dead_neighbour.neighbours, this.state)
-                if(parents.size == 3) {
+                if (parents.size == 3) {
                     this.born.add(dead_neighbour)
                 }
             }
         }
+        this.prev_state = this.state;
+        this.state = new ExtendedSet(this.state);
+        this.state.update(this.born);
+        this.state.diff_update(this.died);
         return this.state
     }
 }
